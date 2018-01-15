1c2d370a5f9b4384cf22cf33a6ad6dcc4f8125e1
Matteo Nardi <m.nardi@maseritalia.com>
2018-01-15 12:44:08 +0100

    Add install script for vim files

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+function link() {
+  echo Linking $1 to $2
+  if [ -L "$1" ]
+  then
+    rm "$1"
+  elif [ -e "$1" ]
+  then
+    rm -ir $2
+  fi
+  ln -sr $2 $1 
+}
+
+link ~/.vimrc vim/vimrc
+link ~/.vim/bundle/my_settings vim
