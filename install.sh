b0396b043f7a4ab5f75a2cbdd643ab2d906571b7
Matteo Nardi <m.nardi@maseritalia.com>
2018-02-27 17:36:46 +0100

    Install phantogen and gruvbox

--- install.sh
+++ install.sh
@@ -17,3 +17,11 @@ function link() {
 link ~/.vimrc vim/vimrc
 link ~/.vim/bundle/my_settings vim
 link ~/.tmux.conf tmux/tmux.conf
+
+if [ ! -f ~/.vim/autoload/pathogen.vim ]
+then
+	mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+	git clone https://github.com/morhetz/gruvbox.git ~/.vim/bundle/gruvbox
+fi
+
