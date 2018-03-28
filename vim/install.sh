9c67df29eb5cfc66eab72575ff39fd927670e403
Matteo Nardi <m.nardi@maseritalia.com>
2018-03-28 10:42:32 +0200

    Install tpope/vim-surround

--- vim/install.sh
+++ vim/install.sh
@@ -31,3 +31,8 @@ then
 	git clone git://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
 fi
 
+if [ ! -d ~/.vim/bundle/vim-surround ]
+then
+	git clone git://github.com/tpope/vim-surround.git ~/.vim/bundle/vim-surround
+fi
+
